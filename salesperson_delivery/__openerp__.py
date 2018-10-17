bb5ee16bd5eb3291f26bbdf0e358eac7bb04a767
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-10-17 11:09:07 +0100

    Add files via upload

---
+++ salesperson_delivery/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "Salesperson on Delivery Form & Slip",
+    'version' : "1.0",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['stock'],
+    'live_test_url': 'https://www.youtube.com/watch?v=2o5nRL_Oxc8',
+    'installable' : True,
+    'description' : "Salesperson on Delivery Slip and Delivery Form",
+    'website': "www.cier.tech",
+    'data': ['views/salesperson_deliverynote.xml', 'views/salesperson_deliveryForm.xml',],
+    'category': 'Warehouse',
+    'license': 'Other proprietary',
+    'summary': 'Salesperson on Delivery Slip and Delivery Form',
+    'price': 10.0,
+    'currency': 'EUR',
+}
---
+++ salesperson_phone/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "Salesperson phone number on Sales Order and Quotation Print",
+    'version' : "1.0",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['sale'],
+    'installable' : True,
+    'description' : "Salesperson's phone number on Sales Order and Quotation Print",
+    'website': "www.cier.tech",
+    'data': ['views/salesperson_phone_so.xml'],
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'summary': "Salesperson's phone number on Sales Order and Quotation Print",
+    'price': 10.0,
+    'currency': 'EUR',
+}
