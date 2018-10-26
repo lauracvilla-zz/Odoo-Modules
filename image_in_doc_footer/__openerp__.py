9f8ee8ff0f06c226552a16b945ea4a898ee9e7bc
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-10-26 16:23:41 +0100

    Add files via upload

---
+++ image_in_doc_footer/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "Image in Document Footer",
+    'version' : "1.0",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['base'],
+    'installable' : True,
+    'description' : "Set your own image as a footer for PDF reports",
+    'website': "www.cier.tech",
+    'data': ['views/custom_footer.xml',],
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'summary': 'Set your own image as a footer for PDF reports',
+
+}
