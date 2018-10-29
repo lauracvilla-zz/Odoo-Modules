5eb46794e82629a6d0db584e22c4bd6857dbdc5a
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-10-29 12:51:22 +0000

    Add files via upload

---
+++ vendor_order_confirmation/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "RFQ - Vendor Confirmation Box",
+    'version' : "1.0",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['purchase'],
+    'installable' : True,
+    'description' : "Vendor signature and confirmation box for Request for Quotation",
+    'website': "https://www.cier.tech",
+    'data': ['views/rfq_signature_box.xml',],
+    'category': 'Purchase',
+    'license': 'Other proprietary',
+    'summary': 'Vendor signature and confirmation box for Request for Quotation',
+    'price': 10.0,
+    'currency': 'EUR',
+}
