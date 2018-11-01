6b937e5d78984175c72469b64a660941d1667bf8
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-11-01 11:09:53 +0000

    Add files via upload
    port from 9.0

---
+++ totals_on_trial_balance/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "Totals on the Trial Balance Report",
+    'version' : "1.0",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['account'],
+    'installable' : True,
+    'description' : "This module adds Total for Credit and Debit on the Trail Balance Report",
+    'website': "www.cier.tech",
+    'data': ['views/total_trial_balance.xml',],
+    'category': 'Accounting',
+    'license': 'Other proprietary',
+    'summary': 'This module adds Total for Credit and Debit on the Trail Balance Report',
+    'price': 10.0,
+    'currency': 'EUR',
+}
