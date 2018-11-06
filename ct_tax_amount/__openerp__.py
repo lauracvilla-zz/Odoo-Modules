10066180081452c34b4d7284e740b3a05ed6f132
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-11-06 10:44:24 +0000

    Add files via upload
    port from 9.0

---
+++ ct_tax_amount/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "Tax Amount on Invoice List",
+    'version' : "1.0",
+    'images': ['images/thumbnail.png'],
+    'author' : "Piotr Cierkosz",
+    'category': 'Accounting',
+    'license': 'Other proprietary',
+    'depends' : ['account'],
+    'installable' : True,
+    'description' : "Display Tax Amount on Invoice List",
+    'website': "https://www.cier.tech",
+    'summary': 'Display Tax Amount on Invoice List',
+    'price': 10.0,
+    'currency': 'EUR',
+    'data': ['views/tax_amount_inv_c.xml',
+    'views/tax_amount_inv_v.xml',
+    ],
+    
+}
