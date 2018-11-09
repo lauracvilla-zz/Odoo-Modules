92f09b394bd83711ab33380d7a2c0f8984eb6abc
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-11-09 10:40:57 +0000

    Add files via upload
    initial commit

---
+++ product_last_price_tab/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+
+{
+    "name": "Product Last Price Tab",
+    "version": "1.0",
+    "category": "Sales",
+    "author": "Piotr Cierkosz",
+    "depends": [
+        "purchase",
+        "sale",
+    ],
+    "data": [
+        "views/product_view.xml",
+    ],
+    'images': ['images/thumbnail.png'],
+    'license': 'Other proprietary',
+    'installable' : True,
+    'description' : "Last Purchase and Sale Price for product variants",
+    'website': "https://www.cier.tech",
+    'summary': 'Last Purchase and Sale Price for product variants',
+    'price': 15.0,
+    'currency': 'EUR',
+    'live_test_url': 'https://www.youtube.com/watch?v=Rx7b3D5dRu8',
+}
