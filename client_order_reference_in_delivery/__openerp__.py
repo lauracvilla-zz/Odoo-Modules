9b0aedcb6756757f5c181fadf547317f74fb3afd
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-11-19 15:55:10 +0000

    Update __openerp__.py

--- client_order_reference_in_delivery/__openerp__.py
+++ client_order_reference_in_delivery/__openerp__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name' : "Customer Reference on Delivery Form & Slip",
+    'name' : "Customer Order Reference on Delivery Form & Slip",
     'version' : "1.0",
     'author' : "Piotr Cierkosz",
     'images': ['images/thumbnail.png'],
     'depends' : ['stock'],
     'installable' : True,
-    'description' : "Customer Reference on Delivery Slip and Delivery Form",
+    'description' : "Customer Order Reference on Delivery Slip and Delivery Form",
     'website': "www.cier.tech",
     'data': ['views/cref_deliverynote.xml', 'views/cref_deliveryForm.xml',],
     'category': 'Warehouse',
     'license': 'Other proprietary',
-    'summary': 'Customer Reference on Delivery Slip and Delivery Form',
+    'summary': 'Customer Order Reference on Delivery Slip and Delivery Form',
     'price': 10.0,
     'currency': 'EUR',
 }
