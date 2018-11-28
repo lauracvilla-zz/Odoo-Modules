c36c9961ae4ca838dce7000740f7d95cd6f7c440
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-11-28 12:48:55 +0000

    Add files via upload

---
+++ your_footer/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "Report Image Footer",
+    'version' : "1.0",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['sale'],
+    'installable' : True,
+    'description' : "Set your own image as a footer for PDF reports",
+    'website': "www.cier.tech",
+    'data': ['views/report_image_footer.xml',],
+    'category': 'Extra Tools',
+    'license': 'Other proprietary',
+    'summary': 'Set your own image as a footer for PDF reports',
+    'price': 9.0,
+    'currency': 'EUR',
+}
