5d848bdfec71a52bf1459c469a2534ebb47f6e27
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-11-28 09:29:37 +0000

    Add files via upload

--- preferred_supplier/__openerp__.py
+++ preferred_supplier/__openerp__.py
@@ -13,7 +13,7 @@
     'images': ['images/thumbnail.png'],
     'license': 'Other proprietary',
     'summary': 'Set a preferred supplier against the product variants',
-    'price': 5.0,
+    'price': 9.0,
     'currency': 'EUR',
     'installable' : True,
     'description' : "Set a preferred supplier against the product variants",
