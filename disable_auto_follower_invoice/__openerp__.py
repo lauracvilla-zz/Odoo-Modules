69fc80573b3151c8259ecf7fda12f788f5a76382
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-11-30 13:06:18 +0000

    Add files via upload

---
+++ disable_auto_follower_invoice/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Disable Auto Followers for Invoices",
+    'summary': """This module disables Partners as auto followers for Invoices.""",
+    'description': """This module disables Partners as auto followers for Invoices.""",
+    'author': "Piotr Cierkosz",
+    'category': 'Sales',
+    'version': '1.0',
+    'images': ['images/thumbnail.png'],
+    'depends' : ['sale'],
+    'installable' : True,
+    'data': [
+        'data/actions.xml',
+    ],
+    'price': 9.0,
+    'currency': 'EUR',
+    'license': 'Other proprietary',
+}
